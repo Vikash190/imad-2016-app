6b39fbe435ebc5506f741bc2218f06851a7fae4c
Vikash190 <singhvikash611@gmail.com>
2016-09-25 11:36:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne={
+    title:" article one| vikash",
+    heading:"Article one",
+    date:"sep 25 2016",
+    content:` <p>
+                content of the page Student ID: 836726997 Student ID: 836726997Student ID: 836726997Student ID: 836726997Student ID: 836726997Student ID: 836726997Student ID: 836726997Student ID: 836726997Student ID: 836726997Student ID: 836726997Student ID: 836726997Student ID: 836726997Student ID: 836726997Student ID: 836726997 
+            </p>
+              <p>
+                content of the page Student ID: 836726997 Student ID: 836726997Student ID: 836726997Student ID: 836726997Student ID: 836726997Student ID: 836726997Student ID: 836726997Student ID: 836726997Student ID: 836726997Student ID: 836726997Student ID: 836726997Student ID: 836726997Student ID: 836726997Student ID: 836726997 
+            </p>`
+    
+};
+
+function createTemplate(data){
+    
+var title=data.title;
+var heading=data.heading;
+var date=data.date;
+var content=data.content;
+var htmlTemplate=`<html>
+    <head>
+    <title>
+      ${title} 
+      
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link href="/ul/style.css" rel="text/css"/>
+    </head>
+    <body>
+    <div>
+        <a href="/">home</a>
+    </div>
+    <hr/>
+    <h3>
+        ${date}
+    </h3>
+    <div>
+        ${heading}
+    </div>
+    <div>
+       ${content}
+    </div>
+    
+    </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
  
 app.get('/article-two', function(req, res){
