64f882290bb98b9add1bf7cfbfc36bf7ec3cca7e
Vikash <singhvikash611@gmail.com>
2016-10-28 02:43:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,10 @@ console.log("loaded");
 var img= document.getElementById("madi");
 var marginLeft=0;
 function moveLeft(){
-    marginLeft=marginLeft-10;
+    marginLeft=marginLeft-1;
     img.style.marginLeft=marginLeft+"px";
 }
 img.onclick=function(){
-    var interval=setInterval(moveLeft,100);
+    var interval=setInterval(moveLeft,50);
     
 };
