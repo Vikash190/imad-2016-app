4cdcd2cc0f1bf5863656699aa04cd377a3174353
Vikash <singhvikash611@gmail.com>
2016-10-31 01:37:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 var express = require('express'); //library to establish connection
 var morgan = require('morgan'); //library help output logs 
 var path = require('path');
-
+var Pool = require('pg').Pool;
 var app=express();
 
 
 app.use(morgan('combined'));
+
+var config = {
+    user :'vikash190',
+    database :'vikash190',
+    host : 'db.imad.hasura-app.io',
+    port : '5432',
+    password : process.env.DB_PASSWORD
+};
 
 var articles={ 
     'article-one':{
@@ -34,6 +42,17 @@ var articles={
     }
 };
 
+var pool = new Pool(config);
+app.get('/test-db', function(req,res){
+    
+    pool.query("SELECT * FROM test", function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.stringify(result));
+        }
+    });
+});
 function createTemplate(data){
     var title=data.title;
     var heading=data.heading;
